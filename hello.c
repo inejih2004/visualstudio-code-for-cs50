7bd50a2c19ec6ea486971758b355fda8b8150235
inejih2004 <154334651+inejih2004@users.noreply.github.com>
2023-12-19 15:07:56 +0000

    Tue, Dec 19, 2023, 3:07 PM +00:00

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<cs50.h>
 int main(void){
-    //string get_string=(jcpjv)
- char x = "a" ;
+    //sgtring get_string=(jcpjv)
+ charx = "a" ;
  printf("hello,%c\n",x);
 
 
