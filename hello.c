c95f12ea37c0b2a42b3f53ad023d081fe9b9f913
inejih2004 <154334651+inejih2004@users.noreply.github.com>
2023-12-19 14:01:22 +0000

    Tue, Dec 19, 2023, 2:01 PM +00:00

--- hello.c
+++ hello.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
 int main(void){
-    printf("hello,world");
+    printf("hello,world\n");
+    printf("hello,world\n");
+    printf("hello,world\n");
+    printf("hello,world\n");
+
 }
