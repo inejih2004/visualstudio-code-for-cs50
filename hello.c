98a4600cbf98d6b2ddf3d5bc8cf629f106ffb394
inejih2004 <154334651+inejih2004@users.noreply.github.com>
2023-12-19 14:08:39 +0000

    Tue, Dec 19, 2023, 2:08 PM +00:00

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<cs50.h>
 int main(void){
-  string answer=get-string("what is your name");
+  string answer=get string("what is your name");
   printf("hello,answer/n");
 
 
